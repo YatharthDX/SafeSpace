9306f5b7cc47b64cd4fc2f7206eeee4d4c078f5b
architag23 <architagoyal506@gmail.com>
2025-03-18 22:30:53 +0530

    try

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom"; //  Wrap the app with BrowserRouter
 import Navbar from "./components/public/navbar"; // Ensure correct path to Navbar
 import "./App.css"; // Import global styles
+// import Home from "./pages/Home.jsx"; // Import Home page
 
 function App() {
   return (
     <BrowserRouter> {/* Required for React Router to work */}
       <div>
-        <Navbar /> {/* Navbar always visible on top */}
+        <Navbar /> 
+        {/* <Home/> */}
         <h1>SafeSpace</h1>
       </div>
     </BrowserRouter>
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+// import React from "react";
+// import Navbar from "../components/Public/navbar";
+
+// const Home = () => {
+//     return (
+//         <div>
+//         <Navbar />
+//         <h1>Homepage</h1>
+//         </div>
+//     );
+//     };
