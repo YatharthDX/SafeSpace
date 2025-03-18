2ddf0294d6a603ec8445b919305914aeab4a5ccc
anirudh802 <basalt802@gmail.com>
2025-03-18 23:17:53 +0530

    Setup Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,23 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom"; //  Wrap the app with BrowserRouter
+import { BrowserRouter,Route,Routes } from "react-router-dom"; //  Wrap the app with BrowserRouter
 import Navbar from "./components/Public/navbar"; // Ensure correct path to Navbar
 import "./App.css"; // Import global styles
 // import Home from "./pages/Home.jsx"; // Import Home page
+import Appointments from "./pages/Appointments.jsx"; // Import the Appointments page
+import Chat from "./pages/Chat.jsx";
+import Profile from "./pages/Profile.jsx";
 
 function App() {
   return (
     <BrowserRouter> {/* Required for React Router to work */}
       <div>
-        <Navbar /> 
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/appointments" element={<Appointments />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
         {/* <Home/> */}
         <h1>SafeSpace</h1>
       </div>
