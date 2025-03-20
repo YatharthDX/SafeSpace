97979ad1be953374d1c9c75d1a612192762895b1
YatharthDX <yaju25sharma@gmail.com>
2025-03-20 23:45:55 +0530

    appointmenthome page done

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, Response, UploadFile, File
 from database.models import EmailRequest, OTPVerification, UserRegistration, PasswordReset, LoginRequest
 from services.auth_service import send_otp_service, verify_otp_service, register_user_service, reset_password_service, login_service
+import base64
+from database.connection import users_collection
+
 
 router = APIRouter()
 
@@ -23,3 +26,23 @@ async def reset_password(request: PasswordReset):
 @router.post("/login")
 async def login(request: LoginRequest, response: Response):
     return login_service(request, response)
+
+@router.post("/upload-profile-picture/")
+async def upload_profile_picture(username: str, file: UploadFile = File(...)):
+    image_data = await file.read()
+    encoded_image = base64.b64encode(image_data).decode("utf-8")
+
+    users_collection.update_one({"name": username}, {"$set": {"profile_picture": encoded_image}}, upsert=True)
+    return {"message": "Profile picture uploaded successfully"}
+
+@router.get("/get-profile-picture/{username}")
+async def get_profile_picture(username: str):
+    user = users_collection.find_one({"name": username})
+    
+    if not user or "profile_picture" not in user:
+        return {"message": "No profile picture found"}
+    
+    image_data = base64.b64decode(user["profile_picture"])
+
+    return Response(content=image_data, media_type="image/jpeg")
+
