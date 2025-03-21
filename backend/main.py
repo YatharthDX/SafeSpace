df28a3b6c93a018a59ee5b44371dd29cb2564644
TallBanana69 <166951711+TallBanana69@users.noreply.github.com>
2025-03-22 01:21:25 +0530

    backend for posts

--- backend/main.py
+++ backend/main.py
@@ -4,11 +4,12 @@ from middleware.auth_middleware import exception_handling
 from routes.auth import router as auth_router
 from routes.user import router as user_router
 from routes.appointments import router as appointments_router
+from routes.posts import router as posts_router
 from database.connection import users_collection, appointments_collection, available_slots_collection, role_requests_collection, redis_client
 from utils.config import EMAIL_USERNAME, EMAIL_PASSWORD
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
@@ -28,6 +29,7 @@ app.middleware("http")(exception_handling)
 app.include_router(auth_router, prefix="/api/auth")
 app.include_router(user_router, prefix="/api")
 app.include_router(appointments_router, prefix="/appointments")  # For prefixed routes
+app.include_router(posts_router, prefix="/blogs")
 
 @app.get("/health")
 async def health_check():
